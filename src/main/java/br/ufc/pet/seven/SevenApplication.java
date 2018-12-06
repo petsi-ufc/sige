4953adebc7e0e9a05e6495b794cab18099385bd6
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-06 16:50:10 -0300

    Projeto gerado com o Spring Initializr

---
+++ src/main/java/br/ufc/pet/seven/SevenApplication.java
@@ -0,0 +1,12 @@
+package br.ufc.pet.seven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenApplication.class, args);
+	}
+}
