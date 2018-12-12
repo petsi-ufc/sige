1c7179a3b913a09b6b7e37a6f6ec905d67526a65
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-12 16:21:27 -0300

    Esboço da arquitetura/api

---
+++ src/main/java/br/ufc/pet/seven/atividade/AtividadeRepository.java
@@ -0,0 +1,7 @@
+package br.ufc.pet.seven.atividade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Integer> {
+
+}
---
+++ src/main/java/br/ufc/pet/seven/evento/EventoRepository.java
@@ -0,0 +1,7 @@
+package br.ufc.pet.seven.evento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventoRepository extends JpaRepository<Evento, Integer> {
+
+}
